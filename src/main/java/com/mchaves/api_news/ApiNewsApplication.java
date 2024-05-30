a6c52954c0b36280edffc243db67df27ea37e424
Mauricio Chaves <feira.nova@gmail.com>
2024-05-30 19:47:51 -0300

    first commit

---
+++ src/main/java/com/mchaves/api_news/ApiNewsApplication.java
@@ -0,0 +1,13 @@
+package com.mchaves.api_news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mchaves/api_news/ApiNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mchaves.api_news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
